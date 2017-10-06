622b2941dc0dc11c736b2d811d98d16cf289d9f4
mariobarbareschi <mario.barbareschi@unina.it>
2017-10-06 15:47:47 +0200

    Update getnova script

--- NOVA/getnova.sh
+++ NOVA/getnova.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 cd /opt
-mkdir NOVA
-cd NOVA
 git clone https://github.com/TUD-OS/NRE
 cd NRE/cross
-./build.sh x86_64
 ./build.sh x86_32
 cd ..
 git submodule init && git submodule update
