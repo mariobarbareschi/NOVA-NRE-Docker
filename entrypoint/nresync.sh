2343fced94f7ada25917ed2fc5d26fa7c31a466f
Michele Basile <mich.basile@gmail.com>
2017-11-06 15:46:08 +0100

    nre sync if needed

--- entrypoint/nresync.sh
+++ entrypoint/nresync.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 if [ ! "$(ls -A /mnt/NRE)" ]; then
-  echo "Found empty NRE directory in host system. Copying sources..."
+  echo "[nresync] Found empty NRE directory in host system. Copying sources..."
   rsync -a /src/NRE/ /mnt/NRE
-  echo "Done."
-else
-  echo "Syncing NRE directory..."
+  echo "[nresync] Done."
+elif [ "$(diff -x .git -r /mnt/NRE /src/NRE)" ]; then
+  echo "[nresync] Found updated NRE directory in host system. Applying changes..."
   rsync -av /mnt/NRE/ /opt/NRE
-  echo "Done."
+  echo "[nresync] Done."
+else
+  echo "[nresync] Same host and container NRE directory. No changes need to be applied."
 fi
-cd $NRE_PATH
+cd /opt/NRE/nre
